48ce7fbfdc36a9ea61baa49d7845acd4b7dde52a
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-10-12 20:26:01 +0800

    tos client

--- internal/service/volcengine/tos_client.go
+++ internal/service/volcengine/tos_client.go
@@ -1,21 +1,34 @@
 package volcengine
 
 import (
-	"fmt"
 	"context"
+	"fmt"
 
 	"github.com/volcengine/ve-tos-golang-sdk/v2/tos"
 
 	"github.com/gogf/gf/v2/frame/g"
 )
 
+var client *tos.ClientV2
+
 func init() {
 	fmt.Println("TOS GO SDK Version: ", tos.Version)
 	ctx := context.Background()
 
-	ak := g.Cfg().MustGet(ctx, "volc.tos.ak").String()
-	sk := g.Cfg().MustGet(ctx, "volc.tos.sk").String()
+	credential := tos.NewStaticCredentials(g.Cfg().MustGet(ctx, "volc.tos.ak").String(), g.Cfg().MustGet(ctx, "volc.tos.sk").String())
+	var err error
+	client, err = tos.NewClientV2(
+		g.Cfg().MustGet(ctx, "volc.tos.endpoint").String(),
+		tos.WithCredentials(credential),
+		tos.WithRegion(g.Cfg().MustGet(ctx, "volc.tos.region").String()),
+	)
 
-	credential := tos.NewStaticCredentials(ak, sk)
+	if err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	}
+}
 
+func GetClient() *tos.ClientV2 {
+	return client
 }

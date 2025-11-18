743beaccd1a3ba698b8dca83a1e9213127df9fd0
Zitong Wu <mr.tom_123@outlook.com>
2025-11-17 21:58:11 -0800

    修改数据库至pgsql语法

--- manifest/migrations/1.sql
+++ manifest/migrations/1.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS transcription (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     task_id TEXT,
     request_id TEXT NOT NULL,
     owner TEXT NOT NULL,
-    file_info JSON,
+    file_info JSONB,
     status TEXT NOT NULL,
-    task_params JSON,
-    audio_transcription_file JSON,
-    chapter_file JSON,
-    information_extraction_file JSON,
-    summarization_file JSON,
-    translation_file JSON,
-    updated_at TEXT NOT NULL DEFAULT (datetime('now')),
-    created_at TEXT NOT NULL DEFAULT (datetime('now'))
+    task_params JSONB,
+    audio_transcription_file JSONB,
+    chapter_file JSONB,
+    information_extraction_file JSONB,
+    summarization_file JSONB,
+    translation_file JSONB,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX idx_transcription_request_id ON transcription(request_id);

39167efa0ba48cc116fd976d901fd9c88b72f583
Zitong Wu <mr.tom_123@outlook.com>
2025-11-28 01:21:43 -0800

    完成delete接口

--- internal/controller/transcription/transcription_v1_delete_task.go
+++ internal/controller/transcription/transcription_v1_delete_task.go
@@ -3,12 +3,22 @@ package transcription
 import (
 	"context"
 
+	v1 "doubao-speech-service/api/transcription/v1"
+	"doubao-speech-service/internal/dao"
+
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
-
-	"doubao-speech-service/api/transcription/v1"
 )
 
 func (c *ControllerV1) DeleteTask(ctx context.Context, req *v1.DeleteTaskReq) (res *v1.DeleteTaskRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	res = &v1.DeleteTaskRes{}
+	if sqlRes, err := dao.Transcription.Ctx(ctx).Where("request_id = ?", req.RequestId).Delete(); err != nil {
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, "删除任务失败")
+	} else if eftRow, err := sqlRes.RowsAffected(); eftRow == 0 {
+		return nil, gerror.New("找不到任务。数据库影响行数为0。")
+	} else if err != nil {
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, "检查任务删除情况失败")
+	}
+	res.Success = true
+	return
 }
